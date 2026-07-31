a996df8f3f5e8ca1570faeccd89a11ecaf3ef219
Jose Roberto Bizaio <jrcontabil.net@gmail.com>
2026-07-31 20:00:18 -0300

    feat(database): cria automaticamente pasta e banco SQLite

--- src/main/java/br/com/intelifiscal/database/initializer/DatabaseInitializer.java
+++ src/main/java/br/com/intelifiscal/database/initializer/DatabaseInitializer.java
@@ -1,23 +1,25 @@
 package br.com.intelifiscal.database.initializer;
 
+import br.com.intelifiscal.constants.DatabaseInfo;
 import br.com.intelifiscal.database.connection.DatabaseConnection;
 import br.com.intelifiscal.database.version.DatabaseVersionManager;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
  * Responsável pela inicialização do banco de dados.
  *
- * Esta classe apenas coordena o processo de criação e atualização
- * do banco de dados. Nenhum SQL deve ser escrito aqui.
- *
  * Fluxo:
  *
- * 1 - Abre conexão.
- * 2 - Executa o controle de versão.
- * 3 - Cria ou atualiza o banco.
- * 4 - Fecha conexão.
+ * 1 - Garante a existência da pasta do banco.
+ * 2 - Abre conexão.
+ * 3 - Executa o controle de versão.
+ * 4 - Cria ou atualiza o banco.
+ * 5 - Fecha conexão.
  *
  * @author José Roberto Bizaio
  */
@@ -31,21 +33,38 @@ public final class DatabaseInitializer {
      */
     public static void initialize() {
 
-        try (Connection connection = DatabaseConnection.getConnection()) {
+        try {
 
-            // Aqui será executado o gerenciamento de versões.
-            //
-            // Exemplo futuro:
-            //
+            createDatabaseDirectory();
 
-            DatabaseVersionManager.initialize(connection);
+            try (Connection connection = DatabaseConnection.getConnection()) {
 
-        } catch (SQLException e) {
+                DatabaseVersionManager.initialize(connection);
+
+            }
+
+        } catch (SQLException | IOException e) {
 
             throw new RuntimeException(
                     "Erro ao inicializar o banco de dados.",
                     e
             );
+
+        }
+
+    }
+
+    /**
+     * Garante que a pasta do banco exista.
+     */
+    private static void createDatabaseDirectory() throws IOException {
+
+        Path directory = Path.of(DatabaseInfo.DATABASE_DIRECTORY);
+
+        if (Files.notExists(directory)) {
+
+            Files.createDirectories(directory);
+
         }
 
     }
